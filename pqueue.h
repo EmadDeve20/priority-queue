180d8bae46fe09de8ddcadb8b44963654bed9fc2
EmadDeve20 <emaddeve20@gmail.com>
2024-07-18 23:59:24 +0330

    update pqueue to set decorator and update add queue function

--- pqueue.h
+++ pqueue.h
@@ -2,6 +2,9 @@
 #define PQUEUE_H_
 #include <stdlib.h>
 
+// check if queue is empty or not
+#define QEMPTY(X) (X==NULL)
+
 extern int QUEUE_LENGTH, MINIMUM_PRORITY, MAXIMUM_PRORITY;
 
 typedef struct pqueue {
@@ -16,7 +19,7 @@ typedef p_queue *pp_queue;
 
 pp_queue create_queue(int value, long priority, void* data);
 
-pp_queue add_queue(pp_queue p, int value, long priority,void* data);
+pp_queue add_queue(pp_queue *p, int value, long priority,void* data);
 
 int pop(pp_queue p, int* value, void* data);
 
